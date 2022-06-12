f1a425a4d1c9dede88ae4b08e420f77c8d8f0e4c
AnhQuoc Le <quocl9396@gmail.com>
2022-06-12 16:35:26 +0700

    Edit code bug

--- Demo_C.c
+++ Demo_C.c
@@ -6,11 +6,7 @@ void main() {
     printf("Hello Quoc!");
     !!!!!!int a = 5;
     printf("Hel!!!!!lo Quoc!");
-    printf("Hello Quoc!");
-    int b = 10;
-    const int c = 20;
 
     // Fix Error
-    // 404
     _getch();
 }
