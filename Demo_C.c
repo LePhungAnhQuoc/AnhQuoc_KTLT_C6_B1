614e54e7b9e90c843718e47137d9d574b3249324
AnhQuoc Le <quocl9396@gmail.com>
2022-06-12 14:23:32 +0700

    Describe what changes in files

---
+++ Demo_C.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+#include "conio.h"
+
+void main() {
+    printf("Hello Quoc!");
+    printf("Hello Quoc!");
+    !!!!!!int a = 5;
+    printf("Hel!!!!!lo Quoc!");
+    printf("Hello Quoc!");
+    printf("Hello!!");
+    printf("Hello Quoc!");
+    _getch();
+}
